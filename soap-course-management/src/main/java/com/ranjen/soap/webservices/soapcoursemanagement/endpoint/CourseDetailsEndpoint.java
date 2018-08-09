614027859865d5b2ded204b5eefe4f70d1261ef3
ranje <ranje@LAPTOP-4IAQGGJI>
2018-08-09 15:48:23 +0800

    Implemented SOAP WS for GetAllCourseDetailsRequest

--- soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/endpoint/CourseDetailsEndpoint.java
+++ soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/endpoint/CourseDetailsEndpoint.java
@@ -1,5 +1,7 @@
 package com.ranjen.soap.webservices.soapcoursemanagement.endpoint;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -7,6 +9,8 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.ranjen.courses.CourseDetails;
+import com.ranjen.courses.GetAllCourseDetailsRequest;
+import com.ranjen.courses.GetAllCourseDetailsResponse;
 import com.ranjen.courses.GetCourseDetailsRequest;
 import com.ranjen.courses.GetCourseDetailsResponse;
 import com.ranjen.soap.webservices.soapcoursemanagement.bean.Course;
@@ -33,7 +37,26 @@ public class CourseDetailsEndpoint {
 	//xml using @ResponsePayload
 	@ResponsePayload
 	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
-		return mapCourse(request);
+		Course course = service.findById(request.getId());
+		return mapCourseDetails(course);
+	}
+	
+	//only getting one course details
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		response.setCourseDetails(mapCourse(course));
+		return response;
+	}
+
+	private CourseDetails mapCourse(Course course) {
+		CourseDetails courseDetails = new CourseDetails();
+
+		courseDetails.setId(course.getId());
+
+		courseDetails.setName(course.getName());
+
+		courseDetails.setDescription(course.getDescription());
+		return courseDetails;
 	}
 	
 	//this method will creating coursedetails , set the values and return back 
@@ -52,6 +75,27 @@ public class CourseDetailsEndpoint {
 		
 		response.setCourseDetails(courseDetails);
 		
+		return response;
+	}
+	
+	@PayloadRoot(namespace = "http://ranjen.com/courses", localPart = "GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processAllCourseDetailsRequest(
+			@RequestPayload GetAllCourseDetailsRequest request) {
+
+		List<Course> courses = service.findAll();
+
+		return mapAllCourseDetails(courses);
+	}
+	
+	//get multiple course details
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		//looping around each course and map each course with coursedetails 
+		for (Course course : courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
 		return response;
 	}
 
