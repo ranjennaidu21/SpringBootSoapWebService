96432bd7d78178342c863afd31fcceb0d29f11ce
ranje <ranje@LAPTOP-4IAQGGJI>
2018-08-09 17:02:16 +0800

    Added SOAP Web Services with WS Security

--- soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
+++ soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
@@ -11,6 +11,10 @@ public class SoapCourseManagementApplication {
 	//Click on the widzler plugin icon and select the method.
 	//Type the input details inside the tag and click go for POST method.
 	//You can see the response return back
+	
+	//Authentication added
+	//So in widzler select authentication and choose WSSE PasswordText authentication
+	//type username and passwrod before click on the POST button
 	public static void main(String[] args) {
 		SpringApplication.run(SoapCourseManagementApplication.class, args);
 	}
--- soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/config/WebServiceConfig.java
+++ soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/config/WebServiceConfig.java
@@ -1,5 +1,8 @@
 package com.ranjen.soap.webservices.soapcoursemanagement.config;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -52,6 +58,33 @@ public class WebServiceConfig extends WsConfigurerAdapter{
 		definition.setLocationUri("/ws");
 		definition.setSchema(coursesSchema);
 		return definition;
+	}
+	
+	//XwsSecurityInterceptor - intercept all the request that coming in , 
+	//check username and id if it is secure
+	@Bean
+	public XwsSecurityInterceptor securityInterceptor(){
+		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+		//Callback Handler -> use the SimplePasswordValidationCallbackHandler bean below
+		//which will check the username and password as given
+		securityInterceptor.setCallbackHandler(callbackHandler());
+		//Security Policy -> securityPolicy.xml to define security policy 
+		//and configure it into this inteceptor
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+		return securityInterceptor;
+	}
+	
+	@Bean
+	public SimplePasswordValidationCallbackHandler callbackHandler() {
+		SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+		handler.setUsersMap(Collections.singletonMap("user", "password"));
+		return handler;
+	}
+
+	//Interceptors.add -> add the XwsSecurityInterceptor bean we have configured above
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(securityInterceptor());
 	}
 
 }
