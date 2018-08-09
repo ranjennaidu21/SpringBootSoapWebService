a026e1434530c97db3102832d1305a12574484cc
ranje <ranje@LAPTOP-4IAQGGJI>
2018-08-09 16:29:58 +0800

    Added SOAP WS to DeleteCourseDetailsRequest using Enum for status

--- soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/endpoint/CourseDetailsEndpoint.java
+++ soap-course-management/src/main/java/com/ranjen/soap/webservices/soapcoursemanagement/endpoint/CourseDetailsEndpoint.java
@@ -9,12 +9,15 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.ranjen.courses.CourseDetails;
+import com.ranjen.courses.DeleteCourseDetailsRequest;
+import com.ranjen.courses.DeleteCourseDetailsResponse;
 import com.ranjen.courses.GetAllCourseDetailsRequest;
 import com.ranjen.courses.GetAllCourseDetailsResponse;
 import com.ranjen.courses.GetCourseDetailsRequest;
 import com.ranjen.courses.GetCourseDetailsResponse;
 import com.ranjen.soap.webservices.soapcoursemanagement.bean.Course;
 import com.ranjen.soap.webservices.soapcoursemanagement.service.CourseDetailsService;
+import com.ranjen.soap.webservices.soapcoursemanagement.service.CourseDetailsService.Status;
 
 
 //tell Spring that this is a ENDPOINT
@@ -97,6 +100,31 @@ public class CourseDetailsEndpoint {
 			response.getCourseDetails().add(mapCourse);
 		}
 		return response;
+	}
+	
+	@PayloadRoot(namespace = "http://ranjen.com/courses", localPart = "DeleteCourseDetailsRequest")
+	@ResponsePayload
+	public DeleteCourseDetailsResponse deleteCourseDetailsRequest(@RequestPayload DeleteCourseDetailsRequest request) {
+		
+		//Intead of returning 1 for success and 0 for failure of deletion
+		//we will return enum String SUCCESS or FAILURE as per the service method.
+		
+		//the status return here is from class CourseDetailsService 
+		Status status = service.deleteById(request.getId());
+
+		DeleteCourseDetailsResponse response = new DeleteCourseDetailsResponse();
+		//the status we will set below is from xsd java object
+		//so we need to map between them.
+		response.setStatus(mapStatus(status));
+
+		return response;
+	}
+	
+	//the status that we returning here is from the CourseDetailsService enum to this jaxb status
+	private com.ranjen.courses.Status mapStatus(Status status) {
+		if (status == Status.FAILURE)
+			return com.ranjen.courses.Status.FAILURE;
+		return com.ranjen.courses.Status.SUCCESS;
 	}
 
 }
